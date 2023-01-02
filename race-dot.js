c5a705e8f8f2cf547e84c1c39bf7cca7cc562861
gupta-parag <parag.gupta@protonmail.com>
2023-01-02 00:19:06 -0500

    Updated Race Dot Map

--- race-dot.js
+++ race-dot.js
@@ -1,11 +1,54 @@
 import cities from "./cities.js";
 
+// Creating objects to toggle layer visibiity
+
+let races = {
+  1: "White Alone",
+  2: "Black or African American Alone",
+  3: "American Indian and Alaska Native alone",
+  4: "Asian Alone",
+  5: "Native Hawaiian and Other Pacific Islander alone",
+  6: "Some Other Race alone",
+  7: "Two or More Races",
+};
+
+let visibility = {
+  1: true,
+  2: true,
+  3: true,
+  4: true,
+  5: true,
+  6: true,
+  7: true,
+};
+console.log(visibility);
+
+//Adding layer buttons to the DOM
+const cntrlPanel = document.querySelector(".control-panel");
+let ul = document.createElement("ul");
+
+for (let k of Object.keys(races)) {
+  let btn = document.createElement("li");
+  let span = document.createElement("span");
+  span.setAttribute("id", k);
+  span.innerText = races[k];
+  //Adding event Function
+  span.addEventListener("click", () => {
+    span.classList.toggle("black");
+    visibility[k] = !visibility[k];
+    render();
+  });
+  btn.append(span);
+  ul.append(btn);
+}
+
+cntrlPanel.append(ul);
+
 // Creating Scatterplot layer by requesting data from github
 // Folder directory has races by number 1-white, 2-black, 3-Native America,
 // 4-Asian Alone, 5-Hawaii or pacific Islander, 6- Some Other Race,
 // 7 - Two or more Races
-
-function createLayer(i = 1, color = [128, 128, 0]) {
+function createLayer(i = 1, color = [128, 128, 0], vis = true) {
   const DATA_URL = `https://raw.githubusercontent.com/gupta-parag/Data-Repo/main/Race-data/Race_${i}.csv`;
   return new deck.ScatterplotLayer({
     id: `scatter-plot-${i}`,
@@ -13,40 +56,35 @@ function createLayer(i = 1, color = [128, 128, 0]) {
     radiusScale: 25,
     radiusMinPixels: 0.25,
     pickable: true,
-    opacity: 0.3,
+    opacity: (i === 3) | (i === 5) | (i === 7) ? 1 : 0.3,
     filled: true,
     getPosition: (d) => [+d.lng, +d.lat, 0],
     getFillColor: color,
     getRadius: 1,
+    pickable: true,
+    visible: vis,
+    autohighlight: true,
   });
 }
 
-let deckgl = new deck.DeckGL({
-  mapStyle:
-    "https://gist.githubusercontent.com/gupta-parag/8b14de2c3c2fd47c3cc2a42d10817f77/raw/995edb143762442c848d5aa3bf3f2cc5681a5525/positronEditOne.json",
-  initialViewState: cities.Chicago,
-  controller: true,
-  layers: [
-    createLayer(4),
-    createLayer(5),
-    createLayer(6),
-    createLayer(7),
-    createLayer(1),
-    createLayer(2),
-    createLayer(3),
-  ],
-});
+function render() {
+  let deckgl = new deck.DeckGL({
+    mapStyle:
+      "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json",
+    initialViewState: cities.Chicago,
+    controller: true,
+    pickable: true,
+    autoHighlight: true,
+    layers: [
+      createLayer(4, [249, 7, 22], visibility[4]),
+      createLayer(5, [255, 177, 0], visibility[5]),
+      createLayer(6, [6, 255, 0], visibility[6]),
+      createLayer(7, [255, 0, 255], visibility[7]),
+      createLayer(2, [246, 107, 14], visibility[2]),
+      createLayer(3, [255, 255, 0], visibility[3]),
+      createLayer(1, [10, 161, 221], visibility[1]),
+    ],
+  });
+}
 
-// const cntrlPanel = document.querySelector(".control-panel");
-// for (let k of Object.keys(cities)) {
-//   let btn = document.createElement("button");
-//   btn.setAttribute("id", k);
-//   btn.innerText = k;
-//   btn.style.margin = "5px";
-//   btn.addEventListener("click", () => {
-//     deckgl.setProps({
-//       initialViewState: cities[k],
-//     });
-//   });
-//   cntrlPanel.append(btn);
-// }
+render();
